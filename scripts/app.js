a56631074e0586cd5ebf0fb6ef93ca35725f7896
Rahat <djrayhan8@gmail.com>
2021-01-24 23:39:51 +0600

    first commit of firebase db used chat

---
+++ scripts/app.js
@@ -0,0 +1,50 @@
+//dom queries
+const chatList = document.querySelector('.chat-list')
+const newChatForm = document.querySelector('.new-chat')
+const updateNameForm = document.querySelector('.new-name')
+const updateMssg = document.querySelector('.update-mssg')
+const rooms = document.querySelector('.chat-rooms')
+
+//add a new chat
+newChatForm.addEventListener('submit', e => {
+    e.preventDefault()
+    const message = newChatForm.message.value.trim()
+    chatroom.addChat(message)
+        .then(() => newChatForm.reset())
+        .catch(err => console.log(err))
+})
+
+//update username
+updateNameForm.addEventListener('submit', e => {
+    e.preventDefault()
+    // update name via chatroom
+    const newName = updateNameForm.name.value.trim()
+    chatroom.updateName(newName)
+    // reset the form
+    updateNameForm.reset()
+    // show then hide the update message
+    updateMssg.innerHTML = `Your name was updated to: <span class="text-bold">${newName}</span>`
+    setTimeout(() => updateMssg.innerText = "", 3000)
+})
+//update the chat room
+rooms.addEventListener('click', e => {
+    // console.log(e)
+    if(e.target.nodeName === 'BUTTON'){
+        chatUI.clear()
+        chatroom.updateRoom(e.target.getAttribute('id'))
+        chatroom.getChat(chat => chatUI.render(chat))
+    }
+})
+
+//check local storage for a name
+const username = localStorage.username ? localStorage.username : 'anon'
+
+// class instances
+const chatUI = new ChatUI(chatList)
+const chatroom = new Chatroom('general', username)
+
+
+
+//get the chat and render
+chatroom.getChat((data) => chatUI.render(data))
+
---
+++ scripts/chat.js
@@ -0,0 +1,59 @@
+// adding new chat documents
+// setting up a real-time listener to get new chats
+// updating the username
+// updating the room
+
+class Chatroom {
+    constructor(room, username){
+        this.room = room
+        this.username = username
+        this.chats = db.collection('chats')
+        this.unsub
+    }
+
+    //add new chat
+    async addChat(message) {
+        //format a chat object for firebase
+        const now = new Date()
+        const chat = {
+            message,
+            username: this.username,
+            room: this.room,
+            created_at: firebase.firestore.Timestamp.fromDate(now)
+        }
+        //save the chat to the database
+        const response = await this.chats.add(chat)
+        return response
+    }
+    //getting chat from the db
+    getChat(callback) {
+        this.unsub = this.chats
+            .where('room', '==', this.room)
+            .orderBy('created_at')
+            .onSnapshot(snapshot => {
+                snapshot.docChanges().forEach(change => {
+                    if(change.type === 'added') {
+                        // update the UI
+                        callback(change.doc.data())
+                    }
+                })
+            })
+    }
+    //updating the name
+    updateName(username) {
+        this.username = username
+        localStorage.setItem('username', username)
+    }
+
+    //update the chat room 
+    updateRoom(room) {
+        this.room = room
+        console.log('room updated');
+        if(this.unsub) {
+            this.unsub()
+        }
+    }
+}
+
+
+
